58b7d4537a2bfe0f0b0cb63738bbd73004ca7f87
Touqi <abdullahalmaruftoki@gmail.com>
2024-02-23 01:01:11 +0600

    some changes added

---
+++ script/ticket.js
@@ -0,0 +1,21 @@
+// function setBg(){
+//     const element = document.getElementsByClassName('btn');
+//     element.style.color = 'red';
+// }
+
+// const element = document.getElementsByClassName("seat");
+
+// function setBg(element) {
+//     element.classlist.add('bg-red');
+// }
+
+// function setColor(){
+// const seat = document.getElementsByClassName('seat')
+//     seat.style.background = 'green';
+// }
+
+const seat = document.getElementById('A1');
+
+seat.addEventListener('click', function onClick() {
+  seat.style.backgroundColor = 'salmon';
+});
